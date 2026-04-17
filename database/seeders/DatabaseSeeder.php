8bbf36af564b47d36f9e4427a06f7c547a6e1199
mehdi <service.sheine@gmail.com>
2026-04-17 02:42:25 +0000

    Seed demo quick-fill accounts during db:seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Seeders\DemoAccessSeeder;
 use App\Models\User;
 use App\Models\Group;
 use App\Models\Student;
@@ -121,5 +122,7 @@ class DatabaseSeeder extends Seeder
                 ]);
             }
         }
+
+        $this->call(DemoAccessSeeder::class);
     }
 }
